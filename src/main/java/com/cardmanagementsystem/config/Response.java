7660f3523c5e1a58edaa71b0121391aef57827cc
ganeshchin <87363278+ganeshchin@users.noreply.github.com>
2022-01-07 23:56:38 +0530

    Update Response.java

--- src/main/java/com/cardmanagementsystem/config/Response.java
+++ src/main/java/com/cardmanagementsystem/config/Response.java
@@ -1,13 +1,10 @@
 package com.cardmanagementsystem.config;
 
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
-
 import com.cardmanagementsystem.model.AddressDetails;
 import com.cardmanagementsystem.model.CardDetails;
 import com.cardmanagementsystem.model.UserDetails;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +17,9 @@ public class Response {
 	private String statusCode;
 	private String statusDescription;
 	private UserDetails userDetails;
-	private AddressDetails addressdetails;
+	private AddressDetails addressDetails;
 	private CardDetails cardDetails;
 	private List<CardDetails> allCardDetails;
 	private List<String> errors;
-
+	
 }
